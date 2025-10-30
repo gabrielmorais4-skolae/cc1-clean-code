7b5c372eff63d88040084ec2e1259f5fe0a73b9e
gabrielmorais4 <gabrielmoraisdev4@gmail.com>
2025-10-30 15:47:27 +0100

    :refacto: simplify numeral conversion logic and improve readability

--- src/roman.js
+++ src/roman.js
@@ -1,38 +1,45 @@
 const mappedNumerals = {
-  "I": 1,
-  "V": 5,
-  "X": 10,
-  "L": 50,
-  "C": 100,
-  "D": 500,
-  "M": 1000,
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
 };
 
 const roman = {
+  resolveNumeralPair(current, next) {
+    if (current < next) {
+      return next - current;
+    } else {
+      return current;
+    }
+  },
+
+  simpleNumeralToInt(numeral) {
+    return mappedNumerals[numeral] || 0;
+  },
+
   romanToInt(numeral) {
     if (numeral.length === 1) {
-      return mappedNumerals[numeral] || 0;
+      return this.simpleNumeralToInt(numeral);
     }
-    
+
     let value = 0;
 
     for (let i = 0; i < numeral.length - 1; i++) {
-      const current = mappedNumerals[numeral[i]];
+      const current = this.simpleNumeralToInt(numeral[i]);
 
-      if (i === numeral.length - 1) {
-        value += current;
+      if (i === numeral.length - 2) {
+        const last = this.simpleNumeralToInt(numeral[i + 1]);
+        value += this.resolveNumeralPair(current, last);
         break;
       }
 
-      const next = mappedNumerals[numeral[i + 1]];
+      const next = this.simpleNumeralToInt(numeral[i + 1]);
 
-      if (current < next) {
-        value = this.romanToInt(numeral.slice(0, i)) +
-               (next - current) +
-               this.romanToInt(numeral.slice(i + 2));
-      } else if (current >= next) {
-        value += current;
-      }
+      value += this.resolveNumeralPair(current, next);
     }
 
     return value;
