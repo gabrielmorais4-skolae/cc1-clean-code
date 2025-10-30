fe90bbd582ad1b8a4b75736a85c8a86efd50515b
gabrielmorais4 <gabrielmoraisdev4@gmail.com>
2025-10-30 15:21:20 +0100

    :feat: implement function to convert base numerals into integers

--- tests/roman.test.js
+++ tests/roman.test.js
@@ -1,31 +1,33 @@
+const roman = require("../src/roman");
+
 describe("Roman numeral converter", () => {
-  describe("Convert basic numerals", () => {
+  describe("Convert basic numerals into", () => {
     it("should convert I to 1", () => {
-      expect(romanToInt("I")).toBe(1);
+      expect(roman.romanToInt("I")).toBe(1);
     });
 
     it("should convert V to 5", () => {
-      expect(romanToInt("V")).toBe(5);
+      expect(roman.romanToInt("V")).toBe(5);
     });
 
     it("should convert X to 10", () => {
-      expect(romanToInt("X")).toBe(10);
+      expect(roman.romanToInt("X")).toBe(10);
     });
 
     it("should convert L to 50", () => {
-      expect(romanToInt("L")).toBe(50);
+      expect(roman.romanToInt("L")).toBe(50);
     });
 
     it("should convert C to 100", () => {
-      expect(romanToInt("C")).toBe(100);
+      expect(roman.romanToInt("C")).toBe(100);
     });
 
     it("should convert D to 500", () => {
-      expect(romanToInt("D")).toBe(500);
+      expect(roman.romanToInt("D")).toBe(500);
     });
 
     it("should convert M to 1000", () => {
-      expect(romanToInt("M")).toBe(1000);
+      expect(roman.romanToInt("M")).toBe(1000);
     });
   });
 });
